48488b1d980e5c3098d1e795c4ded141653339df
ManojBD <manojdissanayake8888@gmail.com>
2022-08-19 23:42:26 +0530

    Upade and Delete Functions For Rooms

--- hotel_management_stm_front_end/src/components/AdminComponents/addRoom/AddRoom.js
+++ hotel_management_stm_front_end/src/components/AdminComponents/addRoom/AddRoom.js
@@ -3,12 +3,13 @@ import validateInfo from '../../../validateInfo'
 import "./addRoom.css"
 import axios from "axios";
 import RoomsData from '../roomsDatatable/RoomsData';
+import AdminNavbar from '../../AdminNavbar';
 //import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 
 const defaultImage = 'https:/icon-library.com/images/no-image-icon/no-image-icon-0.jpg'
 
 const initialFieldValues = {
-    roomId:0,
+    roomID:0,
     roomNumber:'',
     floor:'',
     category:'',
@@ -20,11 +21,16 @@ const initialFieldValues = {
 //abc
 
 export default function AddRoom(props) {
-
-  const {addOrEdit} = props
+    
+  const { addOrEdit, recordForEdit} = props
 
   const [values, setValues] = useState(initialFieldValues)
   const [errors, setErrors] = useState({})
+
+  useEffect(()=>{
+        if(recordForEdit != null)
+            setValues(recordForEdit);
+  }, [recordForEdit])
 
   const handleInputChange = e =>{
         const { name, value } = e.target;
@@ -80,11 +86,11 @@ export default function AddRoom(props) {
     e.preventDefault()
     if(validate()){
         const formData = new FormData()
-        formData.append('roomId', values.roomId)
+        formData.append('roomID', values.roomID)
         formData.append('roomNumber', values.roomNumber)
         formData.append('floor', values.floor)
-        formData.append('status', values.status)
         formData.append('category', values.category)
+         formData.append('status', values.status)
         formData.append('imageFile', values.imageFile)
         addOrEdit(formData, resetForm)
     }
@@ -93,80 +99,95 @@ export default function AddRoom(props) {
   const applyErrorClass = field => ((field in errors && errors[field]==false)?' invalid-field':'')
 
   return (
-    <div className="addRoom">
-        <div className="newContainer">
-           <div className="top" >
-                <h1 style={{ color:'gray', fontSize:"20px" }}>Add New Room</h1>
-           </div>
-            <form  autoComplete='off' noValidate onSubmit={handleFormSubmit}>
-                <div className="bottom">
-                    <div className="left">
-                        <img 
-                            className='image' 
-                            src={values.imageSrc}
-                        />
-                    </div>
-                    <div className="right">
-                        <div className='container1'>
-                            <div className="formInput">
-                                <input 
-                                    accept='image/*' 
-                                    type="file" 
-                                    className={"form-control-file"+ applyErrorClass('imageSrc')}
-                                    onChange={showPreview} 
-                                    id="image-uploader"
-                                />
+    <>
+        <div className="addRoom">
+            <div className="newContainer">
+            <div className="top" >
+                    <h1 style={{ color:'gray', fontSize:"20px" }}>Add New Room</h1>
+            </div>
+                <form  autoComplete='off' noValidate onSubmit={handleFormSubmit}>
+                    <div className="bottom">
+                        <div className="left">
+                            <img 
+                                className='image' 
+                                src={values.imageSrc}
+                            />
+                        </div>
+                        <div className="right">
+                            <div className='container1'>
+                                <div className="formInput">
+                                    <input 
+                                        accept='image/*' 
+                                        type="file" 
+                                        className={"form-control-file"+ applyErrorClass('imageSrc')}
+                                        onChange={showPreview} 
+                                        id="image-uploader"
+                                    />
+                                </div>
+                                <div className="formInput">
+                                    <label>Room Number</label>
+                                    <input 
+                                        className={"form-control"+ applyErrorClass('roomNumber')}
+                                        type="number" 
+                                        name='roomNumber' 
+                                        value={values.roomNumber}
+                                        onChange={handleInputChange}   
+                                    />
+                                </div> 
+                                <div className="formInput">
+                                    <label>Floor</label>
+                                    <input 
+                                        className={"form-control"+ applyErrorClass('floor')}
+                                        type="number" 
+                                        name='floor' 
+                                        value={values.floor}
+                                        onChange={handleInputChange}
+                                    />
+                                </div>
+                                <div className="formInput">
+                                     <label>Category</label>
+                                     <select
+                                        className={"form-control"+ applyErrorClass('category')}
+                                        name='category' 
+                                        value={values.category}
+                                        onChange={handleInputChange}
+                                        
+                                    > 
+                                        <option hidden >-Select-</option>
+                                        <option>Standered Suit</option>
+                                        <option>Luxury Suit</option>
+                                        <option>Honeymoon Suit</option>
+                                        <option>Presidential Suit</option>
+                                    </select>
+                                    
+                                </div>
+                                <div className="formInput">
+                                    <label>Status</label>
+                                    <select 
+                                        className={"form-control"+ applyErrorClass('status')}
+                                        //type="text" 
+                                        name='status' 
+                                        value={values.status}
+                                        onChange={handleInputChange}
+                                    >
+                                        <option hidden >-Select-</option>
+                                        <option>Available</option>
+                                        <option>Booked</option>
+                                        
+                                    </select>
+                                </div>
+                                <div className='col-md-12'>
+                                    <button type='submit' className='btn btn-primary'>Submit</button>
+                                </div>
+                            
                             </div>
-                            <div className="formInput">
-                                <label>Room Number</label>
-                                <input 
-                                    className={"form-control"+ applyErrorClass('roomNumber')}
-                                   // type="text" 
-                                    name='roomNumber' 
-                                    value={values.roomNumber}
-                                    onChange={handleInputChange}   
-                                />
-                            </div> 
-                            <div className="formInput">
-                                <label>Floor</label>
-                                <input 
-                                    className={"form-control"+ applyErrorClass('floor')}
-                                    //type="text" 
-                                    name='floor' 
-                                    value={values.floor}
-                                    onChange={handleInputChange}
-                                />
-                            </div>
-                            <div className="formInput">
-                                <label>Category</label>
-                                <input 
-                                    className={"form-control"+ applyErrorClass('category')}
-                                   // type="text" 
-                                    name='category' 
-                                    value={values.category}
-                                    onChange={handleInputChange}
-                                />
-                            </div>
-                            <div className="formInput">
-                                <label>Status</label>
-                                <input 
-                                    className={"form-control"+ applyErrorClass('status')}
-                                    //type="text" 
-                                    name='status' 
-                                    value={values.status}
-                                    onChange={handleInputChange}
-                                />
-                            </div>
-                            <div className='col-md-12'>
-                                <button type='submit' className='btn btn-primary'>Submit</button>
-                            </div>
-                        
                         </div>
                     </div>
-                </div>
-            </form>
+                </form>
+            </div>
+            
         </div>
-        
-    </div>
-  )
+    </>
+
+    )
 }
