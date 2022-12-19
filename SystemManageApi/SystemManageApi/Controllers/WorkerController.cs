a435369cc02bfdc393baa6357c77e773a16aabc6
ManojBD <manojdissanayake8888@gmail.com>
2022-12-19 19:53:26 +0530

    created worker controller

---
+++ SystemManageApi/SystemManageApi/Controllers/WorkerController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SystemManageApi.Authentication;
+using SystemManageApi.Models;
+using Microsoft.Extensions.Logging;
+
+namespace SystemManageApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkerController : ControllerBase
+    {
+        private readonly WorkerDbContext _context;
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public WorkerController(WorkerDbContext context, IWebHostEnvironment hostEnvironment)
+        {
+            _context = context;
+            this._hostEnvironment = hostEnvironment;
+        }
+
+        // GET: api/Worker
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Worker>>> GetWorkers()
+        {
+            return await _context.Workers
+                .Select(x => new Worker()
+                {
+                    WorkerID = x.WorkerID,
+                    WorkerNumber = x.WorkerNumber,
+                    FirstName = x.FirstName,
+                    LastName = x.LastName,
+                    Email = x.Email,
+                    PhoneNumber = x.PhoneNumber,
+                    Gender = x.Gender,
+                    ImageName = x.ImageName,
+                    ImageSrc = String.Format("{0}://{1}{2}/Images/{3}", Request.Scheme, Request.Host, Request.PathBase, x.ImageName)
+                })
+                .ToListAsync();
+        }
+
+        // GET: api/Worker/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Worker>> GetWorker(int id)
+        {
+            var worker = await _context.Workers.FindAsync(id);
+
+            if (worker == null)
+            {
+                return NotFound();
+            }
+
+            return worker;
+        }
+
+        // PUT: api/Worker/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutWorker(int id, [FromForm] Worker worker)
+        {
+            if (id != worker.WorkerID)
+            {
+                return BadRequest();
+            }
+
+            if (worker.ImageFile != null)
+            {
+                worker.ImageName = await SaveImage(worker.ImageFile);
+
+
+            }
+
+            _context.Entry(worker).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!WorkerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Worker
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Worker>> PostWorker([FromForm] Worker worker)
+        {
+            worker.ImageName = await SaveImage(worker.ImageFile);
+
+            _context.Workers.Add(worker);
+            await _context.SaveChangesAsync();
+
+            return StatusCode(201);
+        }
+
+        // DELETE: api/Worker/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteWorker(int id)
+        {
+            var worker = await _context.Workers.FindAsync(id);
+            if (worker == null)
+            {
+                return NotFound();
+            }
+
+
+            _context.Workers.Remove(worker);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool WorkerExists(int id)
+        {
+            return _context.Workers.Any(e => e.WorkerID == id);
+        }
+
+        [NonAction]
+        public async Task<string> SaveImage(IFormFile imageFile)
+        {
+            string imageName = new String(Path.GetFileNameWithoutExtension(imageFile.FileName).Take(10).ToArray()).Replace(' ', '-');
+            imageName = imageName + DateTime.Now.ToString("yymmssfff") + Path.GetExtension(imageFile.FileName);
+            var imagePath = Path.Combine(_hostEnvironment.ContentRootPath, "Images", imageName);
+            using (var fileStream = new FileStream(imagePath, FileMode.Create))
+            {
+                await imageFile.CopyToAsync(fileStream);
+            }
+
+            return imageName;
+        }
+
+        [NonAction]
+        public void DeleteImage(string imageName)
+        {
+            var imagePath = Path.Combine(_hostEnvironment.ContentRootPath, "Images", imageName);
+            if (System.IO.File.Exists(imagePath))
+                System.IO.File.Delete(imagePath);
+
+        }
+    }
+}
